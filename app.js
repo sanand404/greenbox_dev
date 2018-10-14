5c816d4cf29b03508cad4b7ba07b620a2f58347e
anands-cuelogic <anand.singh@cuelogic.com>
2018-10-14 11:45:18 +0530

    Added Passport

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const dotenv = require("dotenv");
-const app = express();
 
 dotenv.load();
 
-app.get('/',(req, res) => {
-    res.send('Hi there');
-});
+const app = express();
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('accesstoken: ',accessToken);
+    console.log('refreshtoken: ',refreshToken);
+    console.log('profile: ',profile);
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope : ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 app.listen(process.env.PORT || 3000);
