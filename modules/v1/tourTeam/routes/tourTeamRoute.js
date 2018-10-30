d04493ecb12076c86c7ebc3790557488bee2d85d
anands-cuelogic <anand.singh@cuelogic.com>
2018-10-31 01:21:47 +0530

    Implementing Validation on Tour Team

--- modules/v1/tourTeam/routes/tourTeamRoute.js
+++ modules/v1/tourTeam/routes/tourTeamRoute.js
@@ -1,12 +1,14 @@
 import express from "express";
 import passport from "passport";
 import TourTeamController from "../controllers/tourTeamController";
+import TourTeamValidation from "../validators/tourTeamValidation";
 
 const router = express.Router();
 
 router.post(
   "/create",
   passport.authenticate("jwt", { session: false }),
+  TourTeamValidation.tourTeamValidation,
   TourTeamController.create
 );
 
