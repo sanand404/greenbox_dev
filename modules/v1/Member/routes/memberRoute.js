0d74df679df352a7f899b3eb979277049bfacb9c
sannad404 <sanand404@gmail.com>
2018-11-08 22:05:08 +0530

    Member registration with email notification

--- modules/v1/Member/routes/memberRoute.js
+++ modules/v1/Member/routes/memberRoute.js
@@ -1,7 +1,14 @@
 import express from "express";
 import passport from "passport";
+import MemberController from "../controllers/memberController";
+import MemberValidation from "../validations/memberValidation";
 const router = express.Router();
 
-router.post("/register", passport.authenticate("jwt", { session: false }));
+router.post(
+  "/register",
+  passport.authenticate("jwt", { session: false }),
+  MemberValidation.memberValidation,
+  MemberController.create
+);
 
 export default router;
